ff37bfd04cc0f2ce62e4b6561906288b7987b78b
surlejonathan <surle.jonathan@gmail.com>
2022-12-02 17:16:32 +0100

    backend for auth

---
+++ controllers/auth.js
@@ -0,0 +1,60 @@
+import { db } from "../db.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = (req, res) => {
+  // Check existing user
+  const query = "SELECT * FROM users WHERE email = ? OR username = ?";
+  const { email, username, password } = req.body;
+
+  db.query(query, [email, username], (err, result) => {
+    if (err) return res.json(err);
+    if (result.length) return res.status(409).json("User already exists!");
+
+    // Hash password
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    const pass = bcrypt.hashSync("Jonathan", salt);
+
+    console.log("PASS", pass);
+    const addUser =
+      "INSERT INTO users (`username`, `email`, `password`) VALUES (?)";
+    const values = [username, email, hash];
+
+    db.query(addUser, [values], (err, result) => {
+      if (err) return res.json(err);
+      return res.status(200).json("User has been created!");
+    });
+  });
+};
+
+export const login = (req, res) => {
+  // Check user
+  const query = "SELECT * FROM users WHERE username = ?";
+
+  const { username } = req.body;
+
+  db.query(query, username, (err, result) => {
+    if (err) return res.json(err);
+    if (result.length === 0) return res.status(404).json("User not found!");
+
+    // Check password
+
+    const { password, ...other } = result[0];
+
+    const correctPassword = bcrypt.compareSync(req.body.password, password);
+
+    if (!correctPassword) return res.status(400).json("Wrong credentials!");
+
+    const token = jwt.sign(result[0].id, process.env.JWT_KEY);
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
+};
+
+export const logout = (req, res) => {};
---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/users.js";
+import postRouter from "./routes/posts.js";
+import * as dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3010;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server successfully connected on port ${PORT}`);
+});
