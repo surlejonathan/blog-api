3850dfc72bd367a10c85982042d6778132096537
surlejonathan <surle.jonathan@gmail.com>
2022-12-04 23:53:24 +0100

    query to join user and post tables

--- controllers/post.js
+++ controllers/post.js
@@ -14,7 +14,8 @@ export const getPosts = async (req, res) => {
 
 export const getPost = async (req, res) => {
   const postId = req.params.id;
-  const query = "SELECT * FROM posts WHERE id=?";
+  const query =
+    "SELECT p.id, `username`, `title`, `description`, p.image, u.image AS userImage, `category`,`date` FROM users u JOIN posts p ON u.id = p.user_id WHERE p.id = ?";
 
   db.query(query, [postId], (err, results) => {
     if (err) return res.json(err);
