35b47164caad81746a3cdbb9c7e378b7932cddde
surlejonathan <surle.jonathan@gmail.com>
2022-12-14 11:48:40 +0100

    check env

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(express.urlencoded());
 app.use(cors({ credentials: true, origin: process.env.CLIENT_BASE_URL }));
 app.use(cookieParser());
 
+console.log("CLIENT BASE URL", process.env.CLIENT_BASE_URL);
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../client/public/uploads");
